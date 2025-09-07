07c0de85143f30ec34162b0531a4ca57de25645e
deutschich <danlu@gmx.at>
2025-09-07 20:06:53 +0200

    Initial commit

---
+++ src/main/java/com/user404_/tntchat/TNTChat.java
@@ -0,0 +1,17 @@
+package com.user404_.tNTChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TNTChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
